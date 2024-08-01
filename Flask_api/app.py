79451cd7e2c555e27f419721f6789894e1c8c90a
SmitNVaghasiya <smitvaghasiya11280@gmail.com>
2024-08-01 16:31:11 +0530

    updated page layouts

--- Flask_api/app.py
+++ Flask_api/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return render_template('index2.html')
-    return render_template('index.html')
+    return render_template('index2.html')
+    # return render_template('index.html')
 
 
 @app.route('/download', methods=['GET'])
